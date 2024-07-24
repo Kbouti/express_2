57a561e2114e2bda2de7c6d2252b31ef4298eb27
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-07-24 13:13:23 -0700

    Add notes on watch command

--- app.js
+++ app.js
@@ -5,3 +5,10 @@ app.get("/", (req, res) => res.send("Hello, world!"));
 
 const PORT = 3000;
 app.listen(PORT, () => console.log(`My first Express app - listening on port ${PORT}!`));
+
+
+// Start server with command:
+// node app.js
+
+// Start server in watch mode with 
+// node --watch app.js
