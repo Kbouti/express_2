aeee207ea8d062b560d81816885f31b8889a2074
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-07-26 11:38:04 -0700

    Install express-async-handler

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,24 +1,20 @@
-// user controller file - controllers/userController.js
 
-const getUserById = async (req, res) => {
-    const userId = req.params.id;
-  
-    try {
-      const user = await someDBQueryToGetUser(userId);
-  
-      if (!user) {
-        res.status(404).send("User not found");
-        return;
-      }
-  
-      res.send(`User found: ${user.name}`);
-    } catch (error) {
-      console.error("Error retrieving user:", error);
-      res.status(500).send("Internal Server Error");
-  
-      // or we can call next(error) instead of sending a response here
-      // Using `next(error)` however will only render an error page in the express' default view and respond with the whole html to the client.
-      // So we will need to create a special type of middleware function if we want a different response and we will get to that in a bit.
-    }
-  };
-  
+//   Here we install express-async-handler with:
+// npm install --save express-async-handler
+const asyncHandler = require("express-async-handler");
+
+// Any errors that is thrown in this function will automatically be caught and call the `next` function
+const getUserById = asyncHandler(async (req, res) => {
+  const userId = req.params.id;
+
+  const user = await someDBQueryToGetUser(userId);
+
+  if (!user) {
+    res.status(404).send("User not found");
+    return;
+  }
+
+  res.send(`User found: ${user.name}`);
+});
+
+
