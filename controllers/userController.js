c7aec864323611a2a9402ecabb5ea19e4c8e221c
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-07-26 11:15:32 -0700

    Add error handling to user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,13 +3,22 @@
 const getUserById = async (req, res) => {
     const userId = req.params.id;
   
-    const user = await someDBQueryToGetUser(userId);
+    try {
+      const user = await someDBQueryToGetUser(userId);
   
-    if (!user) {
-      res.status(404).send("User not found");
-      return;
+      if (!user) {
+        res.status(404).send("User not found");
+        return;
+      }
+  
+      res.send(`User found: ${user.name}`);
+    } catch (error) {
+      console.error("Error retrieving user:", error);
+      res.status(500).send("Internal Server Error");
+  
+      // or we can call next(error) instead of sending a response here
+      // Using `next(error)` however will only render an error page in the express' default view and respond with the whole html to the client.
+      // So we will need to create a special type of middleware function if we want a different response and we will get to that in a bit.
     }
-  
-    res.send(`User found: ${user.name}`);
   };
   
