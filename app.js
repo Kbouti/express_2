6806445a236a6bfd0b8a9d48ca0acdaad2785fb3
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-07-27 11:10:08 -0700

    Render index view with ejs

--- app.js
+++ app.js
@@ -1,26 +1,32 @@
 // app file
 
-const express = require('express');
-const userRouter = require('./routes/userRoutes');
+const express = require("express");
+const userRouter = require("./routes/userRoutes");
 
 const app = express();
 
+const path = require("path");
+
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-
 
 // application-level middlewares, will always execute on every incoming requests
 // parses form payloads and sets it to the `req.body`
 app.use(express.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
+    console.log('Hello from your middleware function! ');
   // You can of course also create your own for your own use-case!
   // Just make sure to call `next`
   next();
 })
 
+app.get("/", (req, res) => {
+  res.render("index", { message: "EJS rocks!" });
+});
+
 // base mount path is `/users` and will always execute on that specific mount path, and yes including `/users/a/b/c`
-app.use('/users', userRouter);
+app.use("/users", userRouter);
 
 const PORT = 3000;
 app.listen(PORT, () => console.log(`listening on port ${PORT}!`));
