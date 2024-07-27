c2bb816b044a1a5ea8180fdd3161c791b3a5d904
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-07-27 10:55:08 -0700

    Install ejs

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,24 +1,17 @@
 // user controller file - controllers/userController.js
-
-
-
 // ********************************************************************************
 // I'm not sure if this is supposed to be established here or elsewhere, but the customError is used here in this controller so I'll include it
-
-
 class CustomNotFoundError extends Error {
-    constructor(message) {
-      super(message);
-      this.statusCode = 404;
-      // So the error is neat when stringified. NotFoundError: message instead of Error: message
-      this.name = "NotFoundError";
-    }
+  constructor(message) {
+    super(message);
+    this.statusCode = 404;
+    // So the error is neat when stringified. NotFoundError: message instead of Error: message
+    this.name = "NotFoundError";
   }
-  
+}
 // ********************************************************************************
-
-
-
+// Installed ejs with npm install ejs
+// ********************************************************************************
 
 const asyncHandler = require("express-async-handler");
 
